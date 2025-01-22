f66d9be8e0594da14e76e4f20817d69db0269eba
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-01-22 20:32:05 -0300

    feat: update Post struct

--- database/connection.go
+++ database/connection.go
@@ -6,24 +6,9 @@ import (
 	"log"
 	"os"
 
-	_ "github.com/go-sql-driver/mysql" // Importa o driver MySQL
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 )
-
-// Estrutura que representa um post
-type Post struct {
-	ID                 string `json:"id"`
-	Title              string `json:"title"`
-	Content            string `json:"content"`
-	Date               string `json:"date"`
-	Category           string `json:"category"`
-	MetaTagTitle       string `json:"meta_tag_title"`
-	MetaTagDescription string `json:"meta_tag_description"`
-	PostImage          string `json:"post_image"`
-	PostBackground     string `json:"post_background"`
-	Author             string `json:"author"`
-	Keywords           string `json:"keywords"`
-}
 
 func CreateDatabaseConnection() (*sql.DB, error) {
 	err := godotenv.Load("../config/.env")
--- models/post.go
+++ models/post.go
@@ -1,9 +1,15 @@
 package models
 
 type Post struct {
-	ID       string `json:"id"`
-	Title    string `json:"title"`
-	Content  string `json:"content"`
-	Date     string `json:"date"`
-	Category string `json:"category"`
+	ID                 string `json:"id"`
+	Title              string `json:"title"`
+	Content            string `json:"content"`
+	Date               string `json:"date"`
+	Category           string `json:"category"`
+	MetaTagTitle       string `json:"meta_tag_title"`
+	MetaTagDescription string `json:"meta_tag_description"`
+	PostImage          string `json:"post_image"`
+	PostBackground     string `json:"post_background"`
+	Author             string `json:"author"`
+	Keywords           string `json:"keywords"`
 }
