1151941b90e4c051c238836ea943a55ab7a1288b
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-01-22 19:03:11 -0300

    chore: create mysql connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,43 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	router := gin.Default()
 
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatalf("Erro ao carregar o arquivo .env: %v", err)
+	}
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	username := os.Getenv("DB_USERNAME")
+	password := os.Getenv("DB_PASSWORD")
+	database := os.Getenv("DB_NAME")
+
+	// Configurar a string de conexão
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, host, port, database)
+
+	// Abrir a conexão com o banco de dados
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("Erro ao abrir a conexão: %v", err)
+	}
+	defer db.Close()
+
+	// Testar a conexão
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Erro ao conectar ao banco de dados: %v", err)
+	}
+
+	fmt.Println("Conexão bem-sucedida ao banco de dados MySQL!")
 }
