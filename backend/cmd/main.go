0d78d063a6de3c46ae3c546dc4210f472056723f
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-02-08 20:00:18 -0300

    feat: create new Logout validation

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -70,46 +70,6 @@ func register(c *gin.Context) {
 	})
 }
 
-func logout(c *gin.Context) {
-	// Chama a função Authorize para verificar a autenticação e CSRF
-	if err := middlewares.SessionAuthMiddleware(c); err != nil {
-		c.JSON(http.StatusUnauthorized, gin.H{
-			"error": "Unauthorized",
-		})
-		return
-	}
-
-	// Obtém o parâmetro "username" do formulário
-	username := c.DefaultPostForm("username", "")
-	if username == "" {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "Username is required",
-		})
-		return
-	}
-
-	// Acessa o banco de dados para atualizar o usuário, limpando os tokens
-	db := c.MustGet("db").(*sql.DB)
-
-	// Atualiza os valores dos tokens para nulos no banco de dados
-	_, err := db.Exec("UPDATE users SET session_token = ?, csrf_token = ? WHERE username = ?", "", "", username)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "Error while logging out",
-		})
-		return
-	}
-
-	// Limpa os cookies de sessão e CSRF no cliente
-	c.SetCookie("session_token", "", -1, "/", "", true, true) // Cookie com HttpOnly
-	c.SetCookie("csrf_token", "", -1, "/", "", true, false)   // Cookie sem HttpOnly
-
-	// Retorna resposta de sucesso
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Logged out successfully",
-	})
-}
-
 func protected(c *gin.Context) {
 	// Chama a função Authorize para verificar a autenticação e CSRF
 	if err := middlewares.SessionAuthMiddleware(c); err != nil {
@@ -190,10 +150,10 @@ func main() {
 	router.Use(middlewares.CORSMiddleware())
 
 	// Definir rotas
-	router.POST("/register", register)   // Rota de registro
-	router.POST("/login", routes.Login)  // Rota de login
-	router.POST("/logout", logout)       // Rota de logout
-	router.POST("/protected", protected) // Rota de acesso protegido
+	router.POST("/register", register)    // Rota de registro
+	router.POST("/login", routes.Login)   // Rota de login
+	router.POST("/logout", routes.Logout) // Rota de logout
+	router.POST("/protected", protected)  // Rota de acesso protegido
 
 	// Configuração para posts
 	router.GET("/posts", func(c *gin.Context) {
