07ef5bb4769ed8cee776c8eb09c8df013267bb71
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-02-08 18:58:34 -0300

    feat: add register db connection

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"blog-api/models"
 	"blog-api/routes"
 	"blog-api/utils"
+	"database/sql"
 	"errors"
 	"fmt"
 	"log"
@@ -81,30 +82,32 @@ func Authorize(c *gin.Context) error {
 }
 
 func register(c *gin.Context) {
-	if c.Request.Method != http.MethodPost {
-		c.JSON(http.StatusMethodNotAllowed, gin.H{
-			"error": "Invalid method",
-		})
-		return
-	}
+	// Acessando o db do contexto, mas agora como *sql.DB
+	db := c.MustGet("db").(*sql.DB)
 
 	username := c.PostForm("username")
 	password := c.PostForm("password")
 
+	// Verificar se o nome de usuário e a senha têm pelo menos 8 caracteres
 	if len(username) < 8 || len(password) < 8 {
 		c.JSON(http.StatusNotAcceptable, gin.H{
-			"error": "Invalid username / password",
+			"error": "Username and password must be at least 8 characters long",
 		})
 		return
 	}
 
-	if _, ok := users[username]; ok {
+	// Verifica se o nome de usuário já existe no banco de dados
+	var user models.User
+	result := db.QueryRow("SELECT * FROM users WHERE username = ?", username)
+	err := result.Scan(&user.ID, &user.Username, &user.HashedPassword)
+	if err == nil {
 		c.JSON(http.StatusConflict, gin.H{
 			"error": "User already exists",
 		})
 		return
 	}
 
+	// Gerar a senha criptografada
 	hashedPassword, err := hasPassword(password)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
@@ -113,12 +116,14 @@ func register(c *gin.Context) {
 		return
 	}
 
-	users[username] = models.Login{
-		HashedPassword: hashedPassword,
+	// Criar um novo usuário no banco de dados usando Query ou Exec (sem GORM)
+	_, err = db.Exec("INSERT INTO users (username, hashed_password) VALUES (?, ?)", username, hashedPassword)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to register user",
+		})
+		return
 	}
-
-	fmt.Print("hashedPassword", hashedPassword)
-	fmt.Print("users", users)
 
 	c.JSON(http.StatusOK, gin.H{
 		"message": "User registered successfully",
