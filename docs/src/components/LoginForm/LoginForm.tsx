764b16744e4be033c7109a7e0b05b0519ed33b0e
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-02-09 18:35:55 -0300

    feat: add Login route

---
+++ docs/src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,23 @@
+import { StyledLoginForm } from "./LoginFomr.styled";
+
+function LoginForm() {
+  return (
+    <StyledLoginForm>
+      <form action="">
+        <h1>Login</h1>
+        <div className="form-control">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" placeholder="Enter your username" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" placeholder="Enter your password" />
+        </div>
+
+        <button>Submit</button>
+      </form>
+    </StyledLoginForm>
+  );
+}
+
+export default LoginForm;
--- docs/src/pages/login/index.tsx
+++ docs/src/pages/login/index.tsx
@@ -1,8 +1,8 @@
+import LoginForm from "@/components/LoginForm/LoginForm"
+
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <LoginForm />
   );
 }
 
