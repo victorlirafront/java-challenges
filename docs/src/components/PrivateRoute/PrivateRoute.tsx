7ba1baa93a29284d0bdf88700af9abdd45275a5b
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-02-12 21:29:25 -0300

    feat: add PrivateRoute component

---
+++ docs/src/components/PrivateRoute/PrivateRoute.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useAuth } from "@/context/Auth";
+import Cookies from "js-cookie";
+
+const PrivateRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const { isAuthenticated, setIsAuthenticated } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    const sessionToken = Cookies.get("session_token");
+
+    if (!sessionToken) {
+      setIsAuthenticated(false);
+      router.replace("/login");
+    } else {
+      setIsAuthenticated(true);
+    }
+  }, [router, setIsAuthenticated]);
+
+  return isAuthenticated ? <>{children}</> : null;
+};
+
+export default PrivateRoute;
