9faf3351829488a1b322979a6b9d4dad8b1976a8
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-02-16 10:58:45 -0300

    generate build

--- backend/database/connection.go
+++ backend/database/connection.go
@@ -11,28 +11,37 @@ import (
 )
 
 func CreateDatabaseConnection() (*sql.DB, error) {
-	err := godotenv.Load("../config/.env")
+	// Carregar o arquivo .env apenas no ambiente local
+	err := godotenv.Load()
 	if err != nil {
-		log.Fatalf("Erro ao carregar o arquivo .env: %v", err)
+		log.Printf("Aviso: Não foi possível carregar o arquivo .env: %v", err)
 	}
 
+	// Ler as variáveis de ambiente
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
 	username := os.Getenv("DB_USERNAME")
 	password := os.Getenv("DB_PASSWORD")
 	database := os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, host, port, database)
-
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		return nil, fmt.Errorf("Erro ao abrir a conexão: %v", err)
+	// Verificar se todas as variáveis de ambiente estão configuradas
+	if host == "" || port == "" || username == "" || password == "" || database == "" {
+		return nil, fmt.Errorf("uma ou mais variáveis de ambiente não estão configuradas corretamente")
 	}
 
-	// Testar a conexão
+	// Construir string de conexão (DSN)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, host, port, database)
+
+	// Abrir conexão com o banco de dados
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("erro ao abrir a conexão: %v", err)
+	}
+
+	// Testar a conexão com o banco de dados
 	err = db.Ping()
 	if err != nil {
-		return nil, fmt.Errorf("Erro ao conectar ao banco de dados: %v", err)
+		return nil, fmt.Errorf("erro ao conectar ao banco de dados: %v", err)
 	}
 
 	fmt.Println("Conexão bem-sucedida ao banco de dados MySQL!")
