a7f81b54391a567a427d6be407da7e3c152112ce
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-02-16 12:55:03 -0300

    chore: update control origin

--- backend/middlewares/corsMiddleware.go
+++ backend/middlewares/corsMiddleware.go
@@ -10,6 +10,7 @@ func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://blog-api-production-2267.up.railway.app")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://blog-api-two-beta.vercel.app")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
